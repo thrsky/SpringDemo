bee5ce397519f9e7188240fc336700a3c0a2582b
thrsky <1491474343@qq.com>
2017-04-23 22:15:50 +0800

    增加注解

--- girl/src/main/java/com/springboot/thrsky/girl/HelloController.java
+++ girl/src/main/java/com/springboot/thrsky/girl/HelloController.java
@@ -1,9 +1,7 @@
 package com.springboot.thrsky.girl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by thRShy on 2017/4/23.
@@ -12,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 //@RestController = @Controller + @ResponseBody
 //代表了该方法返回的数据用json封装
 @RestController
+//在类上也可以用@RequestMapping
+//比如 @RequestMapping(value="/user/")  那么配合上下面方法上的注解，下面的方法就要用/user/hello 或 /user/hi 来请求了
 public class HelloController {
 
     //spring自动加载对应的bean
@@ -19,8 +19,27 @@ public class HelloController {
     private GirlProperties girlProperties;
 
     //url映射注解
-    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello","/hi"},method = RequestMethod.GET)
     public String say(){
         return girlProperties.getCupSize();
     }
+
+    //@PathVairable 获取URL中的值
+    @RequestMapping(value = "/say/{id}",method = RequestMethod.GET)
+    public String say(@PathVariable("id") int id){
+        return "id:"+id;
+    }
+
+    //如果URL为传统的/user?id=100
+    //那我们该怎么获取这个ID值呢?
+    @RequestMapping(value = "/say",method = RequestMethod.GET)
+    public String say(@RequestParam(value = "id",
+            required = false,//是否必须要传入参数
+            defaultValue = "0"//默认值
+    ) Integer myid){
+        return "id:"+myid;
+    }
+
+    //@GetMapping = @RequsetMapping + method = RequestMethod.GET
+    //@PostMapping 同理
 }
