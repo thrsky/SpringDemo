c5c5df52cab88135a7195aecc7ee9c43737e2151
thrsky <1491474343@qq.com>
2017-04-23 21:56:53 +0800

    注解解析

--- girl/src/main/java/com/springboot/thrsky/girl/HelloController.java
+++ girl/src/main/java/com/springboot/thrsky/girl/HelloController.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by thRShy on 2017/4/23.
  */
+
+//@RestController = @Controller + @ResponseBody
+//代表了该方法返回的数据用json封装
 @RestController
 public class HelloController {
 
+    //spring自动加载对应的bean
     @Autowired
     private GirlProperties girlProperties;
 
+    //url映射注解
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String say(){
         return girlProperties.getCupSize();
