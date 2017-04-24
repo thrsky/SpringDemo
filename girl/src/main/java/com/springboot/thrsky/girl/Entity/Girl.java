9542d57e211a075b7e2d09d38348129cb453d64f
thrsky <1491474343@qq.com>
2017-04-24 21:54:07 +0800

    属性上加上最小值的注解，以便判断

--- girl/src/main/java/com/springboot/thrsky/girl/Entity/Girl.java
+++ girl/src/main/java/com/springboot/thrsky/girl/Entity/Girl.java
@@ -3,6 +3,7 @@ package com.springboot.thrsky.girl.Entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
 
 /**
  * Created by thRShy on 2017/4/24.
@@ -17,6 +18,7 @@ public class Girl {
 
     private String cupSize;
 
+    @Min(value = 18,message = "未成年少女禁止入内")
     private Integer age;
 
     public Girl() {
